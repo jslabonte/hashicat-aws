bd997fb646c4a9c72afa391aa38e9b106cb047ed
jslabonte <jslabonte@gmail.com>
2022-10-27 21:38:04 +0000

    Added s3 bucket module12345

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/InsuranceCo/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "jslinsuranceco"
   acl    = "private"
 
   versioning = {
